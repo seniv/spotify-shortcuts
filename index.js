318837634fe4b5db6ddcf64d9e7438344657d73e
Ivan Seniv <seniv97@gmail.com>
2018-03-11 13:51:46 +0200

    now for all system

--- index.js
+++ index.js
@@ -1,45 +1,154 @@
-const { Spotilocal } = require('spotilocal')
-const { app, globalShortcut, clipboard, Notification } = require('electron')
+const { app, globalShortcut, BrowserWindow, ipcMain } = require('electron')
+const audio = require('win-audio').speaker
+const mic = require('win-audio').mic
+const mediaController = require('./media')
+const express = require('express')
 
-const spotilocal = new Spotilocal()
-
-let spotifyInited = false,
-    paused = false
+let paused = true
+let win
 
 app.on('ready', () => {
-  const pauseShortcut = globalShortcut.register('Alt+Shift+1', () => { // available shortcuts https://electronjs.org/docs/api/accelerator
-    if(!spotifyInited) return false
-    spotilocal.pause(paused)
+  win = new BrowserWindow({
+    width: 200,
+    height: 200,
+    frame: false,
+    movable: false,
+    minimizable: false,
+    maximizable: false,
+    focusable: false,
+    opacity: 0.95,
+    alwaysOnTop: true,
+    transparent: true
+  })
+  win.setIgnoreMouseEvents(true)
+  win.on('closed', () => {
+    win = null
+  })
+  win.loadURL(`file://${__dirname}/index.html`)
+
+
+  globalShortcut.register('Alt+Shift+1', () => {
+    mediaController.executeCommand('previous', () => {})
+  })
+
+  globalShortcut.register('Alt+Shift+2', () => {
+    if(paused) {
+      mediaController.executeCommand('play', () => {})
+    } else {
+      mediaController.executeCommand('pause', () => {})
+    }
     paused = !paused
   })
 
-  const playShortcut = globalShortcut.register('Alt+Shift+2', () => {
-    if(!spotifyInited) return false
-
-    const copiedText = clipboard.readText()
-    if(!copiedText.includes('spotify:track:')) {
-      console.log('here is no spotify URI in clipboard')
-      return false
-    }
-
-    spotilocal.play(copiedText).catch(console.error)
+  globalShortcut.register('Alt+Shift+3', () => {
+    mediaController.executeCommand('next', () => {})
   })
 
-  if (!pauseShortcut) {
-    console.log('pause/play shortcut registration failed')
-  }
+  globalShortcut.register('Alt+Shift+4', () => {
+    actions.toggleMic()
+  })
 
-  if (!playShortcut) {
-    console.log('play copied spotify URI shortcut registration failed')
-  }
+  globalShortcut.register('VolumeUp', () => {
+    actions.increase()
+  })
+  globalShortcut.register('VolumeMute', () => {
+    actions.toggleAudio()
+  })
+  
+  globalShortcut.register('VolumeDown', () => {
+    actions.decrease()
+  })
 })
 
 app.on('will-quit', () => {
   globalShortcut.unregisterAll()
 })
 
-console.log('Initing spotilocal')
-spotilocal.init().then((spotilocal) => {
-  spotifyInited = true
-  console.log('Spolicocal inited')
-}).catch(console.error)
+const server = express()
+server.get('/api/:method', (req, res) => {
+  let returnInfo
+  switch (req.params.method) {
+    case 'previous':
+      mediaController.executeCommand('previous', () => {})
+      res.send({success: 'previous track'})
+      break
+
+    case 'pp': //play/pause
+      if(paused) {
+        mediaController.executeCommand('play', () => {})
+      } else {
+        mediaController.executeCommand('pause', () => {})
+      }
+      paused = !paused
+      res.send({success: 'play/pause'})
+      break
+
+    case 'next':
+      mediaController.executeCommand('next', () => {})
+      res.send({success: 'next track'})
+      break
+
+    case 'increase':
+      returnInfo = actions.increase()
+      break
+
+    case 'decrease':
+      returnInfo = actions.decrease()
+      break
+    
+    case 'toggleaudio':
+      returnInfo = actions.toggleAudio()
+      break
+    case 'togglemic':
+      returnInfo = actions.toggleMic()
+      break
+  
+    default:
+      res.send({error: 'method not found'})
+      break
+  }
+  res.send(Object.assign({ success: true }, returnInfo))
+})
+
+const actions = {
+  increase: () => {
+    audio.increase(10)
+    const data = getData()
+    showInfo(data)
+    return data
+  },
+  decrease: () => {
+    audio.decrease(10)
+    const data = getData()
+    showInfo(data)
+    return data
+  },
+  toggleAudio: () => {
+    audio.toggle()
+    const data = getData()
+    showInfo(data)
+    return data
+  },
+  toggleMic: () => {
+    mic.toggle()
+    const data = getData()
+    showInfo(data, true)
+    return data
+  },
+}
+
+function getData() {
+  return {
+    volume: audio.get(),
+    isAudioMuted: audio.isMuted(),
+    isMicMuted: mic.isMuted()
+  }
+}
+
+function showInfo (data, micToggled = false) {
+  if (!win) return false
+  if (!micToggled) delete data.isMicMuted
+  win.webContents.send('info', data)
+}
+
+server.listen(3228, () => console.log('api started on port 3228'))
